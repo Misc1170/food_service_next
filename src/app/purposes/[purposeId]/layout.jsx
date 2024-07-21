425ea4a0181e5e82006cd6b2bdbdf3706751454f
gtimolyanov <gtimolyanov@rokky.io>
2024-07-21 19:00:31 +0500

    first commit

---
+++ src/app/purposes/[purposeId]/layout.jsx
@@ -0,0 +1,7 @@
+export default function DishesListLayout({children}) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
--- src/pages/dishesPages/DishInfoPage.jsx
+++ src/pages/dishesPages/DishInfoPage.jsx
@@ -1,4 +1,3 @@
-import {useParams} from "react-router-dom";
 import {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import FullRoundedFrame from "../../components/Frames/FullRoundedFrame.jsx";
